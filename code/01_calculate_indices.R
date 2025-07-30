1042ed74ac41988cd2edc3ee8b0f7341ce75934a
ericward-noaa <eric.ward@noaa.gov>
2025-07-30 15:33:36 -0700

    data filtering

--- code/01_calculate_indices.R
+++ code/01_calculate_indices.R
@@ -16,12 +16,14 @@ current_batch <- as.numeric(args[1]) # This gets the batch number
 
 url <- "https://raw.githubusercontent.com/pfmc-assessments/indexwc/main/data-raw/configuration.csv"
 config_data <- read.csv(url, stringsAsFactors = FALSE)
-config_data$max_depth[which(config_data$max_depth=="#NAME?")] = NA
+config_data$max_depth[which(config_data$max_depth=="#NAME?")] = -Inf
 config_data$max_depth <- as.numeric(config_data$max_depth)
-config_data$min_latitude[which(config_data$min_latitude=="#NAME?")] = NA
+config_data$min_latitude[which(config_data$min_latitude=="#NAME?")] = -Inf
 config_data$min_latitude <- as.numeric(config_data$min_latitude)
-config_data$min_year[which(config_data$min_year=="#NAME?")] = NA
+config_data$min_year[which(config_data$min_year=="#NAME?")] = -Inf
 config_data$min_year <- as.numeric(config_data$min_year)
+config_data$max_year[which(config_data$max_year=="#NAME?")] = Inf
+config_data$max_year <- as.numeric(config_data$max_year)
 
 config_data <- dplyr::filter(config_data, source == "NWFSC.Combo")
 # add model index
